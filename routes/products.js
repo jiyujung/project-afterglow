166e39f0181a104c0391469f373ff896a93cc3df
Yeseo Lee <6h15m.develop@gmail.com>
2021-07-01 01:45:11 +0900

    [Update] products.js 단순화

--- routes/products.js
+++ routes/products.js
@@ -15,4 +15,28 @@ router.get('/:p_id', function (req, res, next) {
   })
 });
 
+router.get('/:p_id/reviews', function (req, res, next) {
+    const p_id = req.params.p_id;
+    pool.getConnection(function (err, connection) {
+      const sql = "SELECT * FROM product WHERE p_id=?";
+      connection.query(sql, [p_id], function (err, row) {
+        if (err) console.error(err);
+        res.render('review', { row: row[0] });
+        connection.release();
+      });
+    })
+});
+
+router.get('/:p_id/reviews/form', function (req, res, next) {
+    const p_id = req.params.p_id;
+    pool.getConnection(function (err, connection) {
+      const sql = "SELECT * FROM product WHERE p_id=?";
+      connection.query(sql, [p_id], function (err, row) {
+        if (err) console.error(err);
+        res.render('review_form', { user: req.user });
+        connection.release();
+      });
+    })
+});
+
 module.exports = router;
