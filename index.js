64bc5e39bbc0e55e382e4354fcc8dd37a504dc67
Yeseo Lee <6h15m.develop@gmail.com>
2021-07-01 00:52:15 +0900

    [Update] 기존 category 구조 변경 및 통합

--- index.js
+++ index.js
@@ -1,51 +1,32 @@
-const express   = require('express');
+const express = require('express');
 const app = express();
-const path = require('path')
-const passport  = require('passport');
-const session   = require('express-session');
-const bodyParser = require("body-parser");
-const MySQLStore = require('express-mysql-session')(session);
+const path = require('path');
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
-app.use(session({secret: 'asadlfkj!@#!@#dfgasdg', resave: false, saveUninitialized:true}));
 
-const options = {
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'mysql',
-  database: 'afterglow'
-};
+const session = require('express-session');
+app.use(session({ secret: 'asadlfkj!@#!@#dfgasdg', resave: false, saveUninitialized: true }));
 
-const sessionStore = new MySQLStore(options);
-
-app.use(session({
-  key: 'session_cookie_name',
-  secret: 'session_cookie_secret',
-  store: sessionStore,
-  resave: false,
-  saveUninitialized: false
-}));
-
+const passport = require('passport');
 app.use(passport.initialize());
 app.use(passport.session());
 
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
 // Routes
-app.use('/', require('./routes/main'));
+app.use('/', require('./routes/index'));
 app.use('/auth', require('./routes/auth'));
-app.use('/fragrance', require('./routes/fragrance'));
-app.use('/bathandbody', require('./routes/bathandbody'));
-app.use('/homefragrance', require('./routes/homefragrance'));
-app.use('/review', require('./routes/review'));
+// app.use('/user', require('./routes/user'));
+app.use('/category', require('./routes/category'));
+// app.use('/products', require('./routes/products'));
 
 // Port setting
 const port = 3001;
-app.listen(port, function(){
+app.listen(port, function () {
   console.log(`server on! http://localhost:${port}`);
 });
