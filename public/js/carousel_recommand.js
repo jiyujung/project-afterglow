98549849d8801e50555a2729792e72c38ee7c794
yeseolee <s2019w35@e-mirim.hs.kr>
2021-07-02 11:38:44 +0900

    [Update] 추천 슬라이드 구현

--- public/js/carousel_recommand.js
+++ public/js/carousel_recommand.js
@@ -1,11 +1,8 @@
-var Slider = function(id, _web, _tab, _mobile, spacing){
+var Slider = function(id, _web, _tab, _mobile){
     var containerWidth = 0;
-    var sliderItemWidth = 0;
     var totalCount = 0;
-    var spacing = spacing || 10;
     var display = _web;
     var left = 0;
-    var interval;
   
     var DOM = {
       container: function(id){
@@ -22,13 +19,13 @@ var Slider = function(id, _web, _tab, _mobile, spacing){
         dom.className = 'slider'
         dom.style.position = 'relative';
         dom.style.overflow = 'hidden';
-        dom.style.height = '100%';
+        dom.style.height = '370px';
         dom.style.left = 0;
         dom.style.transition = 'left .5s';
         return dom;
       }
     }
-  
+
     // DOM 만들기
     var container = DOM.container(id);
     var slider = DOM.slider(container);
@@ -40,30 +37,21 @@ var Slider = function(id, _web, _tab, _mobile, spacing){
     for(var i=0; i<items.length; i++){
       items[i].style.float = 'left';
       items[i].style.height = '100%';
-      items[i].style.width = (sliderItemWidth-spacing)+ 'px';
-      items[i].style['margin-right'] =  +'px';
+      items[i].style.width = '700px';
+      items[i].style['margin-right'] = '40px';
     }
   
     // 화면 사이즈 수정시 발생하는 이벤트
     function resize(){
       left = 0;
-      document.querySelector('#'+ id + ' .slider').style.left = left + 'px';
-  
-      var innerWidth = window.innerWidth;
-      if(innerWidth >= 1000){
-        setDisplayCount(_web);
-      }else if(innerWidth < 1000 && innerWidth >= 768) {
-        setDisplayCount(_tab);
-      }else if (innerWidth < 768) {
-        setDisplayCount(_mobile);
-      }
+      document.querySelector('#' + id + ' .slider').style.left = left + 'px';
+      setDisplayCount(_web);
       
       if(display === 1){
-        spacing = 0;
         var items = document.querySelector('#'+ id + ' .slider').children;
         for(var i=0; i<items.length; i++){
-          items[i].style.width = sliderItemWidth + 'px';
-          items[i].style['margin-right'] = 0 + 'px'; // 간격
+          items[i].style.width = '700px';
+          items[i].style['margin-right'] = '40px'; // 간격
         }
       }
     }
@@ -72,13 +60,12 @@ var Slider = function(id, _web, _tab, _mobile, spacing){
     function setDisplayCount(count) {
       display = count;
   
-      containerWidth = container.offsetWidth + spacing;
-      sliderItemWidth = containerWidth / display;
+      containerWidth = container.offsetWidth + 40;
   
-      document.querySelector('#'+ id + ' .slider').style.width = totalCount * sliderItemWidth + spacing * totalCount + 'px';
+      document.querySelector('#'+ id + ' .slider').style.width = (totalCount+1) * 700 + 40 * (totalCount+1) + 'px';
       var items = document.querySelector('#'+ id + ' .slider').children;
       for(var i=0; i<items.length; i++){
-        items[i].style.width = (sliderItemWidth-spacing)+ 'px';
+        items[i].style.width = '700px';
       }
     }
   
@@ -93,11 +80,11 @@ var Slider = function(id, _web, _tab, _mobile, spacing){
     return {
       setDisplayCount: setDisplayCount,
       move: function(index){
-        left = (-1) * sliderItemWidth * index;
+        left = (-1) * 700 * index;
         document.querySelector('#'+ id + ' .slider').style.left = left + 'px';
       },
       prev: function(){
-        left += sliderItemWidth;
+        left += 700;
         var limit = 0;
         if(left > limit){
           left = limit;
@@ -105,8 +92,8 @@ var Slider = function(id, _web, _tab, _mobile, spacing){
         document.querySelector('#'+ id + ' .slider').style.left = left + 'px';
       },
       next: function(){
-        left -= sliderItemWidth;
-        var limit = (-1) * sliderItemWidth * (totalCount - display);
+        left -= 700;
+        var limit = (-1) * 700 * (totalCount - display);
         if(left < limit){
           left = limit;
         }
@@ -115,4 +102,4 @@ var Slider = function(id, _web, _tab, _mobile, spacing){
     }
   }
   
-  var slider = new Slider('slider', 3, 0, 0, 140);
+  var slider = new Slider('slider', 4, 0, 0);
